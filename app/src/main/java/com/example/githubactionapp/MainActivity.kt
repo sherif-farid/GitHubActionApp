d2966fccf1f164a6ed4d918f7f10caaa96609d65
sherf <sherffareed39@gmail.com>
2022-02-24 15:18:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/githubactionapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.githubactionapp
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.githubactionapp.databinding.ActivityMainBinding
+
+@SuppressLint("SetTextI18n")
+class MainActivity : AppCompatActivity() {
+    private var binding : ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        val url =getString(R.string.base_url)
+        val buildType = com.example.githubactionapp.BuildConfig.BUILD_TYPE
+        binding?.envUrl?.text = "buildType $buildType \n\nenvUrl $url"
+    }
+}
