1a7d2e9d84a857d40a44d8d1553545bd2cc22b9b
sherf <sherffareed39@gmail.com>
2022-03-25 10:45:14 +0200

    sae task1

--- app/src/main/java/com/example/githubactionapp/MainActivity.kt
+++ app/src/main/java/com/example/githubactionapp/MainActivity.kt
@@ -14,6 +14,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding?.root)
         val url =getString(R.string.base_url)
         val buildType = com.example.githubactionapp.BuildConfig.BUILD_TYPE
+        // show local environment
         binding?.envUrl?.text = "buildTypeTask1 $buildType \n\nenvUrl $url"
     }
 }
